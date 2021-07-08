ea0ee7f87b6716844b1c17be95ce053cf0c37987
Cesar Valiente <cevalien@microsoft.com>
2021-07-09 00:27:41 +0200

    Added source code files

---
+++ app/src/main/java/com/cesarvaliente/navigationrailsample/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.cesarvaliente.navigationrailsample
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.window.FoldingFeature
+import androidx.window.WindowInfoRepo
+import androidx.window.WindowLayoutInfo
+import com.cesarvaliente.navigationrailsample.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigationrail.NavigationRailView
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var windowInfoRepo: WindowInfoRepo
+    private val scope = MainScope()
+
+    private lateinit var bottomNavView: BottomNavigationView
+    private lateinit var navRailView: NavigationRailView
+    private lateinit var navController: NavController
+
+    @ExperimentalCoroutinesApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        windowInfoRepo = WindowInfoRepo.create(this)
+
+        //Bind components
+        bottomNavView = binding.bottomNavView
+        navRailView = binding.navigationRail
+        navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        setupNavigation()
+        adjustUI()
+    }
+
+    private fun setupNavigation() {
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    private fun adjustUI() {
+        scope.launch {
+            val data = windowInfoRepo.windowLayoutInfo
+            data
+                .onEach { value -> showUI(value) }
+                .collect()
+        }
+    }
+
+    private fun showUI(windowLayoutInfo: WindowLayoutInfo) {
+        if (windowLayoutInfo.displayFeatures.isEmpty()) {
+            bottomNavView.setupWithNavController(navController)
+            bottomNavView.visibility = View.VISIBLE
+            navRailView.visibility = View.GONE
+        } else {
+            (windowLayoutInfo.displayFeatures.component1() as FoldingFeature).apply {
+                if (isSeparating) {
+                    navRailView.setupWithNavController(navController)
+                    navRailView.visibility = View.VISIBLE
+                    bottomNavView.visibility = View.GONE
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scope.cancel()
+    }
+}
